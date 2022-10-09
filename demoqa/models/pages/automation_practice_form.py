65ee5a6d2e8b411c66d5b6f379aecc2812adcb9b
eisaeva <hanymka@me.com>
2022-10-09 22:12:03 +0300

    fix

--- demoqa/models/pages/automation_practice_form.py
+++ demoqa/models/pages/automation_practice_form.py
@@ -12,7 +12,7 @@ from utils import path
 def open_page(url, resourses):
     browser.open(url + resourses)
     ads = ss('[id^=google_ads][id$=container__]')
-    if ads.with_(timeout=6).wait.until(have.size_greater_than_or_equal(3)):
+    if ads.with_(timeout=10).wait.until(have.size_greater_than_or_equal(3)):
         ads.perform(command.js.remove)
 
 
@@ -60,7 +60,7 @@ def select_city(city):
 
 
 def submit():
-    browser.element('#submit').press_enter()
+    browser.element('#submit').press_enter().perform(command.js.scroll_into_view)
 
 
 def data_verification(parameter_name):
